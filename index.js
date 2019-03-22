d7a4d310182468a801ba0f45af299202570387dc
Ricardo Casares <ricardo.casares@grandparade.co.uk>
2019-03-22 12:58:30 +0100

    fix(error): add a comment when errors happen

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const glob = require("globby");
 const { generateFromFile } = require("react-to-typescript-definitions");
 
 module.exports = ({ src, mod = null, git = true }) =>
-  glob.sync(src, { gitignore: git }).map(p => generateFromFile(mod, p));
+  glob.sync(src, { gitignore: git }).map(p => {
+    try {
+      return generateFromFile(mod, p);
+    } catch (e) {
+      return `/* Error: Could not create type definitions for: ${p}\n${
+        e.message
+      }\n*/\n\n`;
+    }
+  });
